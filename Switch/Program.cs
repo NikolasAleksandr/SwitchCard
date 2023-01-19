82b8240156b05f9d6838174e37ddb82fe757b88a
сашка <@->
2023-01-19 22:43:23 +0300

    Добавьте файлы проекта.

---
+++ Switch/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SwitchCard
+{
+    internal class Program
+    {
+        public static object CardLibrary { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaardi loosimine");
+
+            int result = new Random().Next(1,4);
+
+            switch (result)
+            {
+                case 1:
+                    Console.WriteLine("Said poti♠, must kaardimast");
+                    break;
+                case 2:
+                    Console.WriteLine("Said risti♣, must kaardimast");
+                    break;
+                case 3:
+                    Console.WriteLine("Said ruutu♦, punane kaardimast");
+                    break;
+                case 4:
+                    Console.WriteLine("Said ärtu♥, punane kaardimast");
+                    break;
+
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+            }
+        }
+    }
+}
